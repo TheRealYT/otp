69a554085ac1f85a6ed8eb494b142817caf3b806
John Doe <johndoe@example.com>
2025-03-08 19:03:08 +0300

    enable meta-tag generation for production build

--- vite.config.ts
+++ vite.config.ts
@@ -28,5 +28,30 @@ export default defineConfig(({mode, command}) => {
         define: {
             BACKEND_BASE_URL: JSON.stringify(env.VITE_BACKEND_URL),
         },
+        plugins: [
+            {
+                name: 'html-transform',
+                transformIndexHtml(html) {
+                    return html.replace('$$HEADERS$$', `<title>Verify – Unlimited & Free Effortless Phone Verification</title>
+    <meta name="title" content="Verify – Unlimited & Free Effortless Phone Verification"/>
+    <meta name="description"
+          content="Verify provides unlimited, free phone verification with seamless API integration. No SMS costs, with flexible self-hosting or integration options."/>
+
+    <meta property="og:type" content="website"/>
+    <meta property="og:url" content="${env.VITE_URL}/"/>
+    <meta property="og:title" content="Verify – Unlimited & Free Effortless Phone Verification"/>
+    <meta property="og:description"
+          content="Verify provides unlimited, free phone verification with seamless API integration. No SMS costs, with flexible self-hosting or integration options."/>
+    <meta property="og:image" content="${env.VITE_URL}/cover.png"/>
+
+    <meta property="twitter:card" content="summary_large_image"/>
+    <meta property="twitter:url" content="${env.VITE_URL}/"/>
+    <meta property="twitter:title" content="Verify – Unlimited & Free Effortless Phone Verification"/>
+    <meta property="twitter:description"
+          content="Verify provides unlimited, free phone verification with seamless API integration. No SMS costs, with flexible self-hosting or integration options."/>
+    <meta property="twitter:image" content="${env.VITE_URL}/cover.png"/>`);
+                },
+            },
+        ],
     };
 });
